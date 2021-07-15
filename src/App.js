64fd213ecd5c9de8bdf37e209e9582bf5f04ea3f
Andrea Spadavecchia <andrea.spadavecchia@heply.it>
2021-07-15 09:46:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+function App () {
+  const [token, setToken] = useState()
+
+  // this function should be called when the get request from the provider is received.
+  const handleGetToken = () => {
+    // get the provider name from the 'state' query param
+    const provider = Object.fromEntries(new URLSearchParams(window.location.search).entries()).state
+    // call the api with the same query params received from the provider request
+    const params = window.location.search
+    fetch(`http://spedire-platform.test/api/login/${provider}/callback${params}`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response)
+        setToken(response.token)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3><a href='http://localhost/login/facebook'>Facebook</a></h3>
+      <h3><a href='http://localhost/login/google'>Google</a></h3>
+      <h3><a href='http://localhost/login/linkedin'>Linkedin</a></h3>
+      <button type='button' onClick={handleGetToken}>Get token</button>
+      <h2>Token: {token}</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
